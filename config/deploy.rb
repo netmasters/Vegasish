92e5ff2cd5664a522bbb131ef75b3e79af7fb80c
Adam Silverman <silverman.adam@gmail.com>
2015-04-17 08:59:30 -0700

    Update ruby version

--- config/deploy.rb
+++ config/deploy.rb
@@ -35,7 +35,7 @@ set :repo_url, 'git@github.com:netmasters/Vegasish.git'
 # set :keep_releases, 5
 
 set :rbenv_type, :user
-set :rbenv_ruby, '2.2.0'
+set :rbenv_ruby, '2.2.1'
 set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
 set :rbenv_map_bins, %w{rake gem bundle ruby rails}
 set :rbenv_roles, :all
